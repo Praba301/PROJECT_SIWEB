be45f92047a773f8828ba6c178ee48be924187ab
Satyagede-uajy <tristothomas@gmail.com>
2026-04-14 17:09:23 +0700

    update

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -74,7 +74,7 @@ export default function Navbar() {
           background: hoveredBtn ? NEON_PURPLE : 'transparent',
           color: '#fff',
           padding: '8px 20px',
-          // halo
+          // halo gnhgf
           borderRadius: '6px',
           cursor: 'pointer',
           fontSize: '14px',
