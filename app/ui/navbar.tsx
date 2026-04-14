8132925a6c7b560038a44aa612208d2a4d51a044
Satyagede-uajy <tristothomas@gmail.com>
2026-04-14 17:09:09 +0700

    update navbar oleh tristo

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -74,6 +74,7 @@ export default function Navbar() {
           background: hoveredBtn ? NEON_PURPLE : 'transparent',
           color: '#fff',
           padding: '8px 20px',
+          // halo
           borderRadius: '6px',
           cursor: 'pointer',
           fontSize: '14px',
